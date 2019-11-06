9fdf6828940f93047099f07c3983c5d7167adc2c
Clyde <clydeang421@gmail.com>
2019-11-06 10:42:54 +0800

    added javadoc

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -3,7 +3,16 @@
  */
 package Lab1;
 
+/**
+ * Class for demo
+ * @author clydeang421
+ * @version 1.0
+ */
 public class Library {
+	/**
+	 * Main body function
+	 * @param arg does not do anything
+	 */
 	public static void main(String arg[]) {
 		Library lib = new Library();
 		if (lib.someLibraryMethod())
@@ -11,6 +20,15 @@ public class Library {
 		// added comment
 		System.out.println("Added new print.");
 	}
+	/**
+	 * Default Constructor
+	 */
+	public Library () {
+	}
+	/**
+	 * Test Function
+	 * @return always the boolean value true
+	 */
     public boolean someLibraryMethod() {
         return true;
     }
